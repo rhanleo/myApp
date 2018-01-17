31a8c7e3e89e6a5c216e2b791655a5b73690cb4b
Rhan Barredo <rhanbarredo@gamil.com>
2018-01-17 09:46:14 +0800

    New

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
-const app = express();
+const app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
 //Allow us to process data
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 
 //Route
 app.get('/', function(res){
-	res.send("Hi Im a chatbotv1 ");
-});
+	res.send("Hi Im a chatbotv1 ")
+})
 
 
 //Facebook
 app.get('/webhook', function(req, res){
 	if(req.query['hub.verify_token'] === 'rhanbarredo123!'){
-		res.send(req.query['hub.challenge']);
+		res.send(req.query['hub.challenge'])
 	}else{
-		res.send('Wrong Token');
+		res.send('Wrong Token')
 	}
-});
+})
 app.listen(app.get('port'), function(){
-	console.log('running: port');
-});
+	console.log('running: port')
+})
 
 
 
